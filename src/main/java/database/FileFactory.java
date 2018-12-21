0cbfb70b37f0fbf6b7f78f3368ea0d4f45d231ea
Pavel Movchan <ukrolove@gmail.com>
2018-12-21 16:34:01 +0200

    Created FileFactory that will ease testing

---
+++ src/main/java/database/FileFactory.java
@@ -0,0 +1,13 @@
+package database;
+
+import java.io.File;
+
+public class FileFactory {
+    public File make(String path) {
+        return new File(path);
+    }
+
+    public File make(File parent, String child) {
+        return new File(parent, child);
+    }
+}

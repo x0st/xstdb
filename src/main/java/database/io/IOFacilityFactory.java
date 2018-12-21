1ad13d9c06befdb4e7536d9037fd2040fb79f676
Pavel Movchan <ukrolove@gmail.com>
2018-12-21 16:44:07 +0200

    Created a constructor in IOFacilityFactory

--- src/main/java/database/io/IOFacilityFactory.java
+++ src/main/java/database/io/IOFacilityFactory.java
@@ -4,14 +4,18 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class IOFacilityFactory {
-    private int bufferSize = 1024 * 64;
+    private final int mBufferSize;
+
+    public IOFacilityFactory(int bufferSize) {
+        mBufferSize = bufferSize;
+    }
 
     public Reader reader(File file) throws FileNotFoundException {
-        return new Reader(file, bufferSize);
+        return new Reader(file, mBufferSize);
     }
 
     public Writer writer(File file) throws FileNotFoundException {
-        return new Writer(file, bufferSize);
+        return new Writer(file, mBufferSize);
     }
 
     public RAF randomAccessFile(File file) throws FileNotFoundException {

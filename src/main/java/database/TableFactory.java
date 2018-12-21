bdb74d53d8010dc5d693a9687cff3e3a6bb8915f
Pavel Movchan <ukrolove@gmail.com>
2018-12-21 16:39:30 +0200

    Made TableFactory use FileFactory for creating files

--- src/main/java/database/TableFactory.java
+++ src/main/java/database/TableFactory.java
@@ -1,19 +1,26 @@
 package database;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import database.contract.HasTableName;
 
 public class TableFactory {
     private final File mDirPath;
+    private final FileFactory mFileFactory;
 
-    public TableFactory(String path) {
-        mDirPath = new File(path);
+    public TableFactory(String databaseFolder, FileFactory fileFactory) throws FileNotFoundException {
+        mFileFactory = fileFactory;
+        mDirPath = mFileFactory.make(databaseFolder);
+
+        if (!mDirPath.exists()) {
+            throw new FileNotFoundException("Invalid file path");
+        }
     }
 
     public Table make(HasTableName v) {
-        File data = new File(mDirPath, v.getTableName() + ".dat");
-        File definition = new File(mDirPath, v.getTableName() + ".def");
+        File data = mFileFactory.make(mDirPath, v.getTableName() + ".dat");
+        File definition = mFileFactory.make(mDirPath, v.getTableName() + ".def");
 
         return new Table(data, definition);
     }

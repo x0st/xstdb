af3950fbd4d0f5f61caf559011556d0f04066093
Pavel Movchan <ukrolove@gmail.com>
2018-12-30 00:18:19 +0200

    Added a few fabric methods to the BadQueryException class: badSyntax, tableExists, tableNotFound

--- src/main/java/database/exception/BadQueryException.java
+++ src/main/java/database/exception/BadQueryException.java
@@ -4,4 +4,16 @@ public class BadQueryException extends Exception {
     public BadQueryException(String message) {
         super(message);
     }
+
+    public static BadQueryException badSyntax() {
+        return new BadQueryException("Bad syntax");
+    }
+
+    public static BadQueryException tableExists() {
+        return new BadQueryException("A table with this name already exists");
+    }
+
+    public static BadQueryException tableNotFound() {
+        return new BadQueryException("A table with this name does not exist");
+    }
 }

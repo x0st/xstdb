8d946145a043a2fff25278453a9a859edc310b2a
Pavel Movchan <ukrolove@gmail.com>
2018-12-30 21:08:09 +0200

    Deleted the useless exceptions

--- src/main/java/database/exception/ExpressionSyntaxException.java
+++
@@ -1,7 +0,0 @@
-package database.exception;
-
-public class ExpressionSyntaxException extends BadQueryException {
-    public ExpressionSyntaxException(String message) {
-        super(message);
-    }
-}

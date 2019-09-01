4c5232cc5687c9754456618f2886cd08b8a2648d
Pavel Movchan <ukrolove@gmail.com>
2019-09-01 19:14:31 +0300

    Refactored SelectRowsQueryMaker: added some comments; fixed a bug with placeholders not parsed correctly

--- src/main/java/database/query/maker/SelectRowsQueryMaker.java
+++ src/main/java/database/query/maker/SelectRowsQueryMaker.java
@@ -6,7 +6,6 @@ import database.Value;
 import database.contract.LexerInterface;
 import database.contract.QueryMaker;
 import database.exception.BadQueryException;
-import database.exception.BuilderException;
 import database.query.entity.SelectRowsQuery;
 import database.query.expression.PlaceholderObject;
 import database.query.expression.parser.ASTNode;
@@ -18,69 +17,90 @@ public class SelectRowsQueryMaker extends BaseQueryMaker implements QueryMaker<S
     public SelectRowsQuery make(LexerInterface lexer) throws BadQueryException {
         String tableName;
         ASTNode astNode = null;
-        PlaceholderObject object = null;
+        PlaceholderObject predicateValues = null;
 
+        // parse '%table_name%' out of 'get %table_name% %predicate% where %placeholders%'
         tableName = String.valueOf(takeTableName(lexer));
 
+        // if there is predicate
         if (lexer.next()) {
-            astNode = takeExpression(lexer);
-            object = takePlaceholders(lexer);
+            // take the predicate
+            astNode = takePredicate(lexer);
+
+            // between predicate and placeholders there must be a word 'where'
+            lexer.next();
+            assertToken(Token.WORD, lexer);
+            assertLexeme("where", lexer);
+
+            // take values for predicate
+            lexer.next();
+            predicateValues = takePredicateValues(lexer);
         }
 
-        return new SelectRowsQuery(tableName, object, astNode);
+        return new SelectRowsQuery(tableName, predicateValues, astNode);
     }
 
-    @Override
-    public boolean supports(QueryType queryType) {
-        return queryType == QueryType.GET;
-    }
+    /**
+     * Parse '%placeholders%' out of 'get %table_name% %predicate% where %placeholders%'.
+     */
+    private PlaceholderObject takePredicateValues(LexerInterface lexer) throws BadQueryException {
+        assertToken(Token.LEFT_BRACKET, lexer);
 
-    private PlaceholderObject takePlaceholders(LexerInterface lexer) throws BadQueryException {
         PlaceholderObject.Builder builder = PlaceholderObject.builder();
 
         lexer.next();
-        assertToken(Token.LEFT_BRACKET, lexer);
-
-        lexer.next();
+        putPlaceholderIntoBuilder(lexer, builder);
 
         while (true) {
-            assertToken(Token.PLACEHOLDER, lexer);
-
-            builder.placeholder(String.valueOf(lexer.lexeme()));
-
             lexer.next();
 
-            switch (lexer.token()) {
-                case Token.NUMBER:
-                    builder.value(new Value(((Lexer)lexer).lexemeAsInt(), DataType.INTEGER));
-                    break;
-                case Token.WORD:
-                case Token.QUOTED_STRING:
-                    builder.value(new Value(lexer.lexeme(), DataType.STRING));
-                    break;
-                default:
-                    throw BadQueryException.badSyntax();
-            }
+            if (lexer.token() == Token.LOGICAL_OPERATOR) {
+                assertLexeme("&", lexer);
 
-            lexer.next();
-
-            if (lexer.token() == Token.RIGHT_BRACKET) {
+                lexer.next();
+                putPlaceholderIntoBuilder(lexer, builder);
+            } else if (lexer.token() == Token.RIGHT_BRACKET) {
                 break;
+            } else {
+                throw BadQueryException.expectedFound("one of: &, ]", String.valueOf(lexer.lexeme()));
             }
         }
 
-        try {
-            return builder.build();
-        } catch (BuilderException e) {
-            throw BadQueryException.badSyntax();
+        return builder.build();
+    }
+
+    /**
+     * Parse the next placeholder and put it into the given builder.
+     * Example: ':name = alex'. The ':name' and 'alex' are put into the given builder.
+     */
+    private void putPlaceholderIntoBuilder(LexerInterface lexer, PlaceholderObject.Builder builder) throws BadQueryException {
+        assertToken(Token.PLACEHOLDER, lexer);
+
+        builder.placeholder(String.valueOf(lexer.lexeme()));
+
+        lexer.next();
+        assertToken(Token.MATH_OPERATOR, lexer);
+        assertLexeme("=", lexer);
+
+        lexer.next();
+
+        if (lexer.token() == Token.WORD || lexer.token() == Token.QUOTED_STRING) {
+            builder.value(new Value(lexer.lexeme(), DataType.STRING));
+        } else if (lexer.token() == Token.NUMBER) {
+            builder.value(new Value(((Lexer)lexer).lexemeAsInt(), DataType.INTEGER));
+        } else {
+            throw BadQueryException.expectedFound("a value", String.valueOf(lexer.lexeme()));
         }
     }
 
-    private ASTNode takeExpression(LexerInterface lexer) throws BadQueryException {
+    /**
+     * Parse '%predicate%' out of 'get %table_name% %predicate% where %placeholders%'.
+     */
+    private ASTNode takePredicate(LexerInterface lexer) throws BadQueryException {
         assertToken(Token.LEFT_BRACKET, lexer);
 
         lexer.next();
-        ASTNode root = expr(lexer);
+        ASTNode root = takeCondition(lexer);
 
         while (true) {
             lexer.next();
@@ -88,18 +108,22 @@ public class SelectRowsQueryMaker extends BaseQueryMaker implements QueryMaker<S
             if (lexer.token() == Token.LOGICAL_OPERATOR) {
                 root = astNode(Token.LOGICAL_OPERATOR, lexer).setLeft(root);
                 lexer.next();
-                root.setRight(expr(lexer));
+                root.setRight(takeCondition(lexer));
             } else if (lexer.token() == Token.RIGHT_BRACKET) {
                 break;
             } else {
-                throw BadQueryException.badSyntax();
+                throw BadQueryException.expectedFound("one of: |, &, ]", String.valueOf(lexer.lexeme()));
             }
         }
 
         return root;
     }
 
-    private ASTNode expr(LexerInterface lexer) throws BadQueryException {
+    /**
+     * Parse the next condition.
+     * Example: 'name = :name'.
+     */
+    private ASTNode takeCondition(LexerInterface lexer) throws BadQueryException {
         ASTNode buffer = astNode(Token.WORD, lexer);
         lexer.next();
         buffer = astNode(Token.MATH_OPERATOR, lexer).setLeft(buffer);
@@ -109,9 +133,17 @@ public class SelectRowsQueryMaker extends BaseQueryMaker implements QueryMaker<S
         return buffer;
     }
 
+    /**
+     * Just a convenient way to instantiate {@link ASTNode}.
+     */
     private ASTNode astNode(byte token, LexerInterface lexer) throws BadQueryException {
         assertToken(token, lexer);
 
         return new ASTNode(lexer.token(), lexer.lexeme());
+    }
+
+    @Override
+    public boolean supports(QueryType queryType) {
+        return queryType == QueryType.GET;
     }
 }

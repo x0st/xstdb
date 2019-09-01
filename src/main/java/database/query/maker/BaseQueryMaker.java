f73bcf25488c3fe5432cf60e7ba74c19ae85b944
Pavel Movchan <ukrolove@gmail.com>
2019-09-01 19:12:07 +0300

    BaseQueryMaker: added assertLexeme(String expected, LexerInterface lexer); made use of BadQueryException.expectedFound() in the assertToken method

--- src/main/java/database/query/maker/BaseQueryMaker.java
+++ src/main/java/database/query/maker/BaseQueryMaker.java
@@ -1,5 +1,7 @@
 package database.query.maker;
 
+import java.util.Arrays;
+
 import database.contract.LexerInterface;
 import database.exception.BadQueryException;
 import database.query.parser.Token;
@@ -14,7 +16,33 @@ abstract public class BaseQueryMaker {
 
     protected void assertToken(byte expected, LexerInterface lexer) throws BadQueryException {
         if (lexer.token() != expected) {
-            throw BadQueryException.badSyntax();
+            String expectedMessage = null;
+
+            if (expected == Token.MATH_OPERATOR) {
+                expectedMessage = "one of: <>, >, <, =";
+            } else if (expected == Token.PLACEHOLDER) {
+                expectedMessage = "a placeholder";
+            } else if (expected == Token.WORD || expected == Token.QUOTED_STRING) {
+                expectedMessage = "a string";
+            } else if (expected == Token.LEFT_BRACKET) {
+                expectedMessage = "a [";
+            } else if (expected == Token.RIGHT_BRACKET) {
+                expectedMessage = "a ]";
+            } else if (expected == Token.NUMBER) {
+                expectedMessage = "a number";
+            } else if (expected == Token.LOGICAL_OPERATOR) {
+                expectedMessage = "one of: |, &";
+            } else {
+                expectedMessage = "a token of type " + expected;
+            }
+
+            throw BadQueryException.expectedFound(expectedMessage, String.valueOf(lexer.lexeme()));
+        }
+    }
+
+    protected void assertLexeme(String expected, LexerInterface lexer) throws BadQueryException {
+        if (Arrays.hashCode(lexer.lexeme()) != Arrays.hashCode(expected.toCharArray())) {
+            throw BadQueryException.expectedFound(expected, String.valueOf(lexer.lexeme()));
         }
     }
 }

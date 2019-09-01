d85d2ab9ac43b070b6e3d808adde99e763289942
Pavel Movchan <ukrolove@gmail.com>
2019-09-01 19:06:57 +0300

    Extended BadQueryException: new method expectedFound(String expected, String found)

--- src/main/java/database/exception/BadQueryException.java
+++ src/main/java/database/exception/BadQueryException.java
@@ -9,6 +9,10 @@ public class BadQueryException extends Exception {
         return new BadQueryException("Bad syntax");
     }
 
+    public static BadQueryException expectedFound(String expected, String found) {
+        return new BadQueryException("Bad query. Expected " + expected + ", but found " + found + ".");
+    }
+
     public static BadQueryException tableExists() {
         return new BadQueryException("A table with this name already exists");
     }
